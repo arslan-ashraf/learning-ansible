5a2aedf501f70b41c1330a4f0154eaa38d98640a
arslan-ashraf <arslanash@gmail.com>
2026-09-26 16:13:05 -0400

    changes

---
+++ 02-lineinfile-module/docker-commands.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/bash
+
+
+docker build -t my-ansible-core .
+
+
+# runs the playbook
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  my-ansible-core \
+  ansible-playbook -i inventory.yaml playbook.yaml
+
+
+# dry run, it test runs the playbook but without making any
+# actual changes
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  my-ansible-core \
+  ansible-playbook -i inventory.yaml playbook.yaml --check
+
+
+# shows what changes the playbook will make without actually
+# making them, useful when used with modules like copy, template,
+# lineinfile, etc.
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  my-ansible-core \
+  ansible-playbook -i inventory.yaml playbook.yaml --diff
+
+
+# run playbook with different variables, variable doesn't even
+# need to be defined in the playbook, it can be simply used but
+# then must be passed in the command
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  my-ansible-core \
+  ansible-playbook \
+  --extra-vars="{fruit: orange, vegetable: spinach}" \
+  -i inventory.yaml playbook.yaml
+
+
+# increases verbosity of prints, -v is the default
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  my-ansible-core \
+  ansible-playbook -vv -i inventory.yaml playbook.yaml
+
+
+# run only tasks with the given tags, --tag flag can also take
+# keywords such as tagged, untagged, or multiple --tag flags
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  my-ansible-core \
+  ansible-playbook \
+  --tag fruit_tag,example_tag \  # --skip-tags <tag_name>
+  -i inventory.yaml playbook.yaml
+
+
+# list all tags
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  my-ansible-core \
+  ansible-playbook \
+  -i inventory.yaml playbook.yaml \
+  --list-tags
+
+
+# running with SSH
+docker run --rm -it \
+  -v $(pwd):/ansible \
+  -v ~/.ssh:/root/.ssh:ro \
+  my-ansible-core \
+  ansible-playbook -i inventory.yaml playbook.yaml
